8b784a41d31e59e9c33d6b96ce832257231b7da2
rezaarifandee <rezaarifandee@gmail.com>
2019-05-08 02:17:56 +0700

    Add help and argument error handling.
    When no arguments specified, the program gives you segfault. This commit
    fixes that. Now the program will display help message when no arguments
    given.
    
    I also added options for argument. Now when we input "-h" as argument,
    the program ignores all other arguments and display help message then
    quit.

--- src/main.c
+++ src/main.c
@@ -3,14 +3,42 @@
 #include"include/curl/getip.h"
 #include"include/file/appendhosts.h"
 
+void printhelp(){
+		printf("Usage: [options] url\n\n");
+
+		printf("Options:\n");
+		printf("-h\t	Display this message and exit.\n\n");
+
+		printf("For reporting bugs or requesting features, please visit:\n");
+		printf("<https://github.com/rezaarifandee/ezhostsfile>\n");
+}
+
+
 int main(int argc, char **argv){
-	printf("%s\n", argv[1]);
-	char *ip = getip(argv[1]); //get ip from api
-	char *stringtoappend=malloc(strlen(ip) + 2 + strlen(argv[1]));
-	stringtoappend[0] = '\0';
-	strcat(stringtoappend, ip);
-	strcat(stringtoappend, " ");
-	strcat(stringtoappend, argv[1]);
-	if(appendtofile("/etc/hosts", stringtoappend) == 0) printf("Entry added successfully\n");
+	int helpflag=0;
+
+	if(argc==1){
+		printhelp();
+	}
+	else{
+		for(int i = 1; i < argc; i++){
+			if(strcmp(argv[i],"-h")==0){
+				helpflag=1;
+				break;
+			}
+		}
+
+		if(helpflag) printhelp();
+		else{
+			printf("%s\n", argv[1]);
+			char *ip = getip(argv[1]); //get ip from api
+			char *stringtoappend=malloc(strlen(ip) + 2 + strlen(argv[1]));
+			stringtoappend[0] = '\0';
+			strcat(stringtoappend, ip);
+			strcat(stringtoappend, " ");
+			strcat(stringtoappend, argv[1]);
+			if(appendtofile("/etc/hosts", stringtoappend) == 0) printf("Entry added successfully\n");
+		}
+	}
 	return 0;
 }
